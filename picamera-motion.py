6d8610de12903f01d62516ceb749d1da33b7be14
Claude Pageau <pageauc@gmail.com>
2018-02-20 05:50:13 -0600

    Release 2.3 renaming and added more comments

--- picamera-motion.py
+++ picamera-motion.py
@@ -29,7 +29,7 @@ except ImportError:
     print("ERROR : Could Not Import settings.py")
     exit(1)
 
-PROG_VER = "ver 2.2"
+PROG_VER = "ver 2.3"
 PROG_PATH = os.path.abspath(__file__)
 BASE_DIR = PROG_PATH[0:PROG_PATH.rfind("/")+1]
 BASE_FILE_NAME = PROG_PATH[PROG_PATH.rfind("/")+1:PROG_PATH.rfind(".")]
@@ -49,33 +49,42 @@ def get_now():
     return now
 
 #------------------------------------------------------------------------------
-def check_image_path(image_path):
-    """ if image_path does not exist create the folder """
-    if not os.path.isdir(image_path):
+def check_image_dir(image_dir):
+    """ if image_dir does not exist create the folder """
+    if not os.path.isdir(image_dir):
         if verbose:
-            print("INFO  : Creating Image Storage folder %s" % (image_path))
+            print("INFO  : Creating Image Storage folder %s" % (image_dir))
         try:
-            os.makedirs(image_path)
+            os.makedirs(image_dir)
         except OSError as err:
-            print("ERROR : Could Not Create Folder %s %s" % (image_path, err))
+            print("ERROR : Could Not Create Folder %s %s" % (image_dir, err))
             exit(1)
 
 #------------------------------------------------------------------------------
-def get_file_name(image_path, image_name_prefix, current_count):
-    """ Create a file name based on settings.py variables """
+def get_file_name(image_dir, image_name_prefix, current_count):
+    """
+    Create a file name based on settings.py variables
+    Note image numbering will not be saved so restarting
+    will restart image numbering and overwrite previous images.
+    Set imageNumOn=False to save image in datetime format to
+    make image name unique and avoid overwriting previous image.
+
+    See pi-timolo.py getCurrentCount function for using glob to
+    extract the most recent number counter from saved images.
+    """
     if imageNumOn:
-        # could use os.path.join to construct file image path
-        file_name = image_path + "/" + image_name_prefix + str(current_count) + ".jpg"
+        # you could also use os.path.join to construct image path file_path
+        file_path = image_dir+ "/"+image_name_prefix+str(current_count)+".jpg"
     else:
         right_now = datetime.datetime.now()
-        file_name = ("%s/%s%04d%02d%02d-%02d%02d%02d.jpg"
-                     % (image_path, image_name_prefix,
+        file_path = ("%s/%s%04d%02d%02d-%02d%02d%02d.jpg"
+                     % (image_dir, image_name_prefix,
                         right_now.year, right_now.month, right_now.day,
                         right_now.hour, right_now.minute, right_now.second))
-    return file_name
+    return file_path
 
 #------------------------------------------------------------------------------
-def take_day_image(filename):
+def take_day_image(image_path):
     """ Take a picamera image """
     with picamera.PiCamera() as camera:
         camera.resolution = (imageWidth, imageHeight)
@@ -88,11 +97,11 @@ def take_day_image(filename):
         camera.exposure_mode = 'auto'
         camera.awb_mode = 'auto'
         time.sleep(1)
-        camera.capture(filename)
+        camera.capture(image_path)
     if verbose:
         print("%s INFO  : takeDayImage (%ix%i) - Saved %s"
-              % (get_now(), imageWidth, imageHeight, filename))
-    return filename
+              % (get_now(), imageWidth, imageHeight, image_path))
+    return image_path
 
 #------------------------------------------------------------------------------
 def get_stream_array():
@@ -114,21 +123,23 @@ def scan_motion():
     while True:
         data2 = get_stream_array()
         diff_count = 0
-        for h in range(0, streamHeight):
-            for w in range(0, streamWidth):
-                # get the diff of the pixel. Conversion to int
+        for y in range(0, streamHeight):
+            for x in range(0, streamWidth):
+                # get pixel differences. Conversion to int
                 # is required to avoid unsigned short overflow.
-                diff = abs(int(data1[h][w][1]) - int(data2[h][w][1]))
+                diff = abs(int(data1[y][x][1]) - int(data2[y][x][1]))
                 if  diff > threshold:
                     diff_count += 1
                     if diff_count > sensitivity:
-                        return w, h
+                        # x,y is a very rough motion position
+                        return x, y
         data1 = data2
 
 #------------------------------------------------------------------------------
-def motion_detection():
-    """ Loop until motion found then take an image,
-        and continue motion detection. ctrl-c to exit
+def do_motion_detection():
+    """
+    Loop until motion found then take an image,
+    and continue motion detection. ctrl-c to exit
     """
     current_count = imageNumStart
     while True:
@@ -136,20 +147,22 @@ def motion_detection():
         if verbose:
             print("%s INFO  : Motion Found At xy(%i,%i) in stream wh(%i,%i)"
                   % (get_now(), x, y, streamWidth, streamHeight))
-        filename = get_file_name(imagePath, imageNamePrefix, current_count)
+        file_name = get_file_name(imagePath, imageNamePrefix, current_count)
         if imageNumOn:
             current_count += 1
-        take_day_image(filename)
+        take_day_image(file_name)
 
 # Start Main Program Logic
 if __name__ == '__main__':
-    check_image_path(imagePath)
-    if verbose:
-        print("%s INFO  : Scan for Motion "
-              "threshold=%i (diff)  sensitivity=%i (num px's)..."
-              % (get_now(), threshold, sensitivity))
+    check_image_dir(imagePath)
+    print("%s INFO  : Scan for Motion "
+          "threshold=%i (diff)  sensitivity=%i (num px's)..."
+          % (get_now(), threshold, sensitivity))
+    if not verbose:
+        print("%s WARN  : Messages turned off per verbose = %s"
+              % (get_now(), verbose))
     try:
-        motion_detection()
+        do_motion_detection()
     except KeyboardInterrupt:
         print("")
         print("INFO  : User Pressed ctrl-c")
